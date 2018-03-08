8f04d5f845a57d9274f3b4e976893aa4bccdf767
Lindsey Trainor <ltrainor38@gmail.com>
2018-03-08 02:40:14 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -25,39 +25,39 @@ def prime?(num)
   end
 end
 =======
-if num < 2 
-  false 
+if num < 2
+  false
 elsif num.even?
-  false  
-else 
-   primes = Array.new 
-  primes << 2 
-  primes << 3 
+  false
+else
+   primes = Array.new
+  primes << 2
+  primes << 3
   i = 5
-  old_prime = false  
-  
-  while i < 2000 
+  old_prime = false
+
+  while i < 2000
     primes.each do |prime|
-      old_prime = i % prime != 0 
+      old_prime = i % prime != 0
     end
-    if old_prime  
+    if old_prime
       primes << i
-    end 
-    i += 2  
-  end 
+    end
+    i += 2
+  end
   if primes.include?(num)
-    primes 
-  else 
-    primes    
-  end 
-end 
+    primes
+  else
+    primes
+  end
+end
 
 
 
 
- 
- 
-  
-  
-  
+
+
+
+
+
   
